66aa2df13f0fc4973e828d4fd2c18268fab95874
siva <sivamss5882@gmail.com>
2024-04-04 14:52:21 +0530

    add to css in Phones.css

--- src/comonpents/Phones.jsx
+++ src/comonpents/Phones.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+import './Phones.css'
 function Phones() {
   const [phone, setPhone] = useState(null);
   const { id } = useParams();
@@ -25,15 +25,42 @@ function Phones() {
   }
 
   return (
-    <div className='phone-container'>
-      <div className='card'>
-      
+    <div className='phone1-container'>
+      <div className='card1'>
+      <div className='img2'>
         <img src={phone.img} alt={phone.name} 
-        className='img' />
-         <h1 className='name'>{phone.name}</h1>
-         <p>{phone.Specifications.map((res)=> (
-          <p><h4 className='h4'>{res.name}</h4> :{res.description}</p>
+        className='img1'/>
+        <div className='price1'>
+
+          <div className='n'>
+          <h4 className='name1'>{phone.name}</h4>
+          <h2>$ : {phone.price} 25,000 31% off</h2>
+          </div>
+
+          <div className='highlights'>
+          <p>
+          <h2>Higlights : </h2> {phone.highlights}</p>
+          </div>
+
+          <h2>Features :</h2>
+         <div className='Features'>
+          <p>{phone.features.map((res)=> (
+          <p><h4 className='h4'>{res.name}</h4> : {res.description}</p>
          ))}</p>
+              </div>
+
+          <h2>Specification :</h2>
+          <div className='Specification'>
+          <p>{phone.Specifications.map((res)=> (
+          <p><h4 className='h4'>{res.name}</h4> : {res.description}</p>
+         ))}</p>
+         </div>
+        
+          </div>
+        </div>
+       
+        
+         
       </div>
     </div>
   );
